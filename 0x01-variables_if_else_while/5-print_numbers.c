3c82be5b849c3204e4f564fe4ca24eea7be2ad7d
rufus wells <rufusmfmwellens@gmail.com>
2022-06-03 12:30:23 -0700

    Done

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print all numbers of base 10, starting from 0.
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		putchart(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
