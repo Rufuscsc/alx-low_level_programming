d86f056fc6d0ff258a08a1464e4a9d18f99248ca
rufus wells <rufusmfmwellens@gmail.com>
2022-06-09 07:22:33 -0700

    Update

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers between 0 to 9.
+ * Return: no return.
+ */
+void print_numbers(void)
+{
+	int c = 48;
+
+	while(c <= 57)
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+
+}
